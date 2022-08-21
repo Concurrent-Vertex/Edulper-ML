61bf8fc3a2272ca6f2aaf14bbb8abdc1503f0cbb
Yashashwini Dixit <yashashwinid@valuefy.com>
2022-08-21 23:55:18 +0530

    added ignore and text summarizer

--- text-summarizer.py
+++ text-summarizer.py
@@ -10,3 +10,7 @@ elif(c=="1"):
     original_text=input("Enter the text to summarize: ")
 summary_text = summarization(original_text)[0]['summary_text']
 print("Summary:", summary_text)
+
+'''
+Tropical rainforests are rainforests that occur in areas of tropical rainforest climate in which there is no dry season – all months have an average precipitation of at least 60 mm – and may also be referred to as lowland equatorial evergreen rainforest. True rainforests are typically found between 10 degrees north and south of the equator (see map); they are a sub-set of the tropical forest biome that occurs roughly within the 28-degree latitudes (in the equatorial zone between the Tropic of Cancer and Tropic of Capricorn). Within the World Wildlife Fund's biome classification, tropical rainforests are a type of tropical moist broadleaf forest (or tropical wet forest) that also includes the more extensive seasonal tropical forests.
+'''
